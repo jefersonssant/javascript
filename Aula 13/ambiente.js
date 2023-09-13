9075c9ce7ff8e320ea9fff370201aee99d17c44d
jefersonssant <jefss@outlook.com>
2023-09-12 22:01:04 -0300

    Pasta Aula 13 criada

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,23 @@
+var contador = 1
+do {
+    console.log('Tudo bem?')
+    contador = contador + 1 //c++
+} while (contador <= 6) 
+
+
+
+
+/*var contador = 1
+while (contador <= 6) {
+    console.log('Tudo bem?')
+    contador = contador + 1 //c++
+}*/
+
+
+
+/*console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')*/
