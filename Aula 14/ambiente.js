598918157b1efe5444f27f2011997cf781d88346
jefersonssant <jefss@outlook.com>
2023-09-15 14:38:25 -0300

    Pastas aula 14 e exercícios

---
+++ Aula 14/ambiente.js
@@ -0,0 +1,13 @@
+/*var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++ // c = c + 1 ou c += 1
+}*/
+
+//------------------------------
+
+console.log ('Vai começar')
+for (var c=1; c <= 5; c++) {
+    console.log(c)
+}
+console.log ('Fim')
